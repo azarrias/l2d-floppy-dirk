0f2bd6a0ef5ed0be7436fb7d16e4b92af61ffda1
azarrias <adolfo.zarrias@gmail.com>
2019-04-30 21:07:56 +0200

    Setup window and render both ground and background drawables

---
+++ main.lua
@@ -0,0 +1,40 @@
+push = require 'libs.push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+GROUND_HEIGHT = 16
+
+-- local variable; won't be accessible outside from this file
+local background = love.graphics.newImage('assets/background.png')
+local ground = love.graphics.newImage('assets/ground.png')
+
+function love.load()
+  -- use nearest-neighbor (point) filtering on upscaling and downscaling to prevent blurring of text and 
+  -- graphics instead of the bilinear filter that is applied by default 
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  love.window.setTitle('Floppy Dirk')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    vsync = true,
+    fullscreen = false,
+    resizable = true
+  })
+end
+
+function love.resize(w, h)
+  push:resize(w, h)
+end
+
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
+
+function love.draw()
+  push:start()
+  love.graphics.draw(background, 0, 0)
+  love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - GROUND_HEIGHT)
+  push:finish()
+end
